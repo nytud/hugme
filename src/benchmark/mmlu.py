f86b3b7fee3ffc08525231cc6cb75adac54c6f25
matyasosvath <matyasosv@gmail.com>
2025-01-30 12:10:20 +0100

    update

--- src/benchmark/mmlu.py
+++ src/benchmark/mmlu.py
@@ -1,10 +1,10 @@
-
+from tqdm import tqdm
 
 import config
 import helper
 
 
-def benchmark(args, generation_pipeline) -> None:
+def benchmark(args, generation_pipeline) -> float:
     dataset = helper.read_json(config.DATASETS + "mmlu.json")
     dataset = preprocess(dataset)
     results = generate_results(args, generation_pipeline, dataset)
@@ -22,12 +22,34 @@ def preprocess(dataset: dict):
 
 
 def generate_results(args, generation_pipeline, dataset):
-    pass
+    results = []
+    for entry in tqdm(dataset, desc="Generating responses", unit="query"):
+        question, target = entry['input'], entry['target']
+        a, b, c, d = entry['A'], entry['B'], entry['C'], entry['D']
+        query = (
+            "Alább van egy kérdés, és négy válasz. Kizárólag a helyes választ előtti betűt add vissza!"
+            f"Kérdés: {question} Válaszok: {a}, {b}, {c}, {d}"
+        )
+        prompt = helper.get_model_prompt(args.model_name, query)
+        output = generation_pipeline(prompt)[0]['generated_text']
+
+        print("prompt, output")
+        print(prompt, output)
+
+        results.append({"query": question, "actual_output": output, "target": target})
+    return results
 
 
 def compute_score(args, results):
-
-    score = 0.0
-
-    return score
-
+    score = 0
+    for entry in tqdm(results, desc="Calculating scores", unit="query"):
+        if entry['output'].strip() == entry['target']:
+            entry['score'] = 1
+            score += 1
+        else:
+            entry['score'] = 0
+    total_score = score / len(results)
+    print(f"MMLU score: {score}")
+    if args.save_results:
+        helper.save_json(results, config.RESULTS_DIR, "mmlu-results.json")
+    return total_score
