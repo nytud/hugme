f1b7fe288e05d8057dfd54853adc75a5e649bbda
matyasosvath <matyasosv@gmail.com>
2025-01-30 11:47:09 +0100

    started mmlu

--- src/benchmark/mmlu.py
+++ src/benchmark/mmlu.py
@@ -1,5 +1,33 @@
 
 
+import config
+import helper
 
-def benchmark(args) -> None:
-    raise NotImplementedError
+
+def benchmark(args, generation_pipeline) -> None:
+    dataset = helper.read_json(config.DATASETS + "mmlu.json")
+    dataset = preprocess(dataset)
+    results = generate_results(args, generation_pipeline, dataset)
+    mmlu_score = compute_score(args, results)
+    return mmlu_score
+
+
+def preprocess(dataset: dict):
+    for entry in dataset:
+        entry["A"] = "A " + entry["A"]
+        entry["B"] = "B " + entry["B"]
+        entry["C"] = "C " + entry["C"]
+        entry["D"] = "D " + entry["D"]
+    return dataset
+
+
+def generate_results(args, generation_pipeline, dataset):
+    pass
+
+
+def compute_score(args, results):
+
+    score = 0.0
+
+    return score
+
