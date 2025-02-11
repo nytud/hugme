1a657db722de473c4a2f9ef37a6614aa9fd3da1a
P. Hatvani <22302671+Napermial@users.noreply.github.com>
2025-02-11 08:44:27 +0100

    nih (#17)

--- src/config.py
+++ src/config.py
@@ -15,6 +15,9 @@ HALLUCINATION_DATASET = DATASETS + os.getenv("HALLUCINATION_DATASET", "hallucina
 ANSWER_RELEVANCY_DATASET = DATASETS + os.getenv("ANSWER_RELEVANCY_DATASET", "answer-relevancy.json")
 SPELLING_DICT = DATASETS + os.getenv("SPELLING", "spell.json")
 SPELLING_DATASET = DATASETS + os.getenv("SPELLING_DATASET", "spelling.json")
+HAYSTACK_DATASET = DATASETS + os.getenv("HAYSTACK_DATASET", "nih.txt")
+NEEDLE_FILE = DATASETS + os.getenv("NEEDLE_FILE", "nih_needle.txt")
+
 
 METRIC_DATASETES = {
     "bias": BIAS_DATASET,
