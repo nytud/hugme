c77ada00646c8a28138de9200190d0bb14961a5b
matyasosvath <matyasosv@gmail.com>
2025-09-04 16:23:49 +0200

    fix prompt alignment error when using openai generation

--- src/benchmark/prompt_alignment.py
+++ src/benchmark/prompt_alignment.py
@@ -5,39 +5,36 @@ import helper
 from deepeval.test_case import LLMTestCase
 from deepeval.metrics import PromptAlignmentMetric
 
+import template
+
 
 MAX_NEW_TOKENS = 256
 
 
-def compute_metric(task_name, args, generate):
-    
-    # load Dataset  
+def compute_metric(task_name, args, generate_fn):
+
     dataset = helper.read_json(config.PROMPT_ALIGNMENT_DATASET)
 
     sample_size = max(1, int(args.sample_size * len(dataset)))
     dataset = random.sample(dataset, sample_size)
 
-    # generate
     results = []
     for entry in dataset:
         prompt_instructions = entry['prompt_instructions']
-        query = entry['query']
+        query = template.get_prompt("prompt-alignment", entry)
 
-        
-        actual_output = generate(
+        actual_output = generate_fn(
             query,
             max_new_tokens=MAX_NEW_TOKENS,
             alpaca_prompt=args.use_alpaca_prompt
         )
 
-        
         results.append({
             "query": str(query),
             "actual_output": str(actual_output),
             "prompt_instructions": prompt_instructions
         })
 
-    # Mérés 
     threshold = 0.5
     total_score = 0.0
     passed = 0
@@ -59,12 +56,11 @@ def compute_metric(task_name, args, generate):
         if metric.score >= threshold:
             passed += 1
 
-    # Eredmények kiértékelése
     avg_score = total_score / len(results)
     success_rate = passed / len(results)
 
-    print(f"Átlagos pontszám: {avg_score:.2f}")
-    print(f"Sikerráta ({threshold}+): {success_rate:.2%}")
+    print(f"Average score: {avg_score:.2f}")
+    print(f"Success rate ({threshold}+): {success_rate:.2%}")
 
 
     if args.save_results:
