e5d3e72a0971d56dea54eebfc496efac4659d4c6
Gunics Roland <grolcsi09@gmail.com>
2026-04-24 14:15:47 +0200

    fix(cultural): fixex missing the preprocess step in prompt generation

--- src/benchmark/cultural.py
+++ src/benchmark/cultural.py
@@ -12,6 +12,7 @@ def compute_metric(args, task_name: str) -> dict:
     dataset = helper.read_json(config.CULTURAL_DATASET)
     sample_size = max(1, int(args.sample_size * len(dataset))) # at least 1 sample
     dataset = random.sample(dataset, sample_size)
+    dataset = preprocess(dataset)
     gen_results = generation.generate_results(args, task_name, dataset, format_result)
     return compute_scores(args, gen_results)
 
@@ -59,12 +60,12 @@ def compute_scores(args, results: list):
             entry['score'] = 0.0
     total_score = score / len(results)
 
-    logging.info(f"MMLU benchmark score: {round(total_score * 100, 2)}%")
+    logging.info(f"Cultural benchmark score: {round(total_score * 100, 2)}%")
     if args.save_results:
         helper.save_json(
             results,
             config.RESULTS_DIR,
-            f"{config.MMLU}-{args.model_name}-{args.thinking}-eval-results.json"
+            f"{config.CULTURAL}-{args.model_name}-{args.thinking}-eval-results.json"
         )
 
     return helper.group_by_category(results, total_score)
